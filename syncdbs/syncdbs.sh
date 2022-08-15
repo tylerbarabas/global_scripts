bb06cac861676ba0e15cd43590bab7c6c5b501f0
Tyler Barabas <tylerbarabas@gmail.com>
2022-08-15 14:36:03 -0500

    gtyProd -> gty1

--- syncdbs/syncdbs.sh
+++ syncdbs/syncdbs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rsync gtyProd:/var/www/db_dump/* ~/db_dump
-rsync gtyProd:/var/www/dev_db_dump/* ~/dev_db_dump
+rsync gty1:/var/www/db_dump/* ~/db_dump
+rsync gty1:/var/www/dev_db_dump/* ~/dev_db_dump
